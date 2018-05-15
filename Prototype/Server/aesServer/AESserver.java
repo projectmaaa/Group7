bb450cf367cb299a7cc080f098090cefce7f9df5
ArkadyKo <ArkadyKo@ArkadyKoretsky>
2018-05-16 00:33:39 +0300

    Class AESserver was created

---
+++ Prototype/Server/aesServer/AESserver.java
@@ -0,0 +1,53 @@
+package aesServer;
+
+import ocsf.server.AbstractServer;
+import ocsf.server.ConnectionToClient;
+
+public class AESserver extends AbstractServer {
+
+	final public static int defaultPort = 3306;
+
+	public AESserver(int port) {
+		super(port);
+	}
+
+	@Override
+	protected void handleMessageFromClient(Object msg, ConnectionToClient client) {
+		System.out.println("Message received: " + msg + " from " + client);
+		switch ((String) msg) {
+		case "Edit or Remove":
+			break;
+		}
+	}
+
+	/**
+	 * This method overrides the one in the superclass. Called when the server
+	 * starts listening for connections.
+	 */
+	protected void serverStarted() {
+		System.out.println("Server listening for connections on port " + getPort());
+	}
+
+	/**
+	 * This method overrides the one in the superclass. Called when the server stops
+	 * listening for connections.
+	 */
+	protected void serverStopped() {
+		System.out.println("Server has stopped listening for connections.");
+	}
+
+	public static void main(String[] args) {
+		int port = 0; // Port to listen on
+		try {
+			port = Integer.parseInt(args[0]); // Get port from command line
+		} catch (Throwable t) {
+			port = defaultPort; // Set port to 5555
+		}
+		AESserver server = new AESserver(port);
+		try {
+			server.listen(); // Start listening for connections
+		} catch (Exception ex) {
+			System.out.println("ERROR - Could not listen for clients!");
+		}
+	}
+}
