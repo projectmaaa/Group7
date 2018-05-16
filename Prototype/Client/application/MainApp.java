92428937f2e502cc45bb95ad61384677533f940e
morac <morac@DESKTOP-E65KSPH>
2018-05-16 20:00:58 +0300

    check

--- Prototype/Client/application/MainApp.java
+++ Prototype/Client/application/MainApp.java
@@ -23,7 +23,7 @@ public class MainApp {
 		try {
 			host = args[0];
 		} catch (ArrayIndexOutOfBoundsException e) {
-			host = "localhost"; //mori
+			host = "localhost"; //mor
 		}
 		MainApp userApp = new MainApp(host, defaultPort);
 		LoginWindow.go();
